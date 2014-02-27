f4a5f05a9b2e24655b78b58779f95d491e657eec
unknown <Juan_M_Medina@WN7X64-D78QFZ1.aus.amer.dell.com>
2014-02-27 11:49:01 -0600

    Adding Shell Modules

---
+++ Modules/Shell/Shell.psm1
@@ -0,0 +1,35 @@
+
+
+function Compare-Folders
+{
+  param
+  (
+    [Parameter()][ValidateNotNullOrEmpty()][string] $SourceFolder=$(throw "Base Folder is required"),
+    [Parameter()][ValidateNotNullOrEmpty()][string] $TargetFolder=$(throw "Target Folder is required")
+  )
+
+  try
+  {
+    if (-not (Test-Path $SourceFolder))
+    {
+      throw "The Source Folder does not exist"
+    }
+    if (-not (Test-Path $TargetFolder))
+    {
+      throw "The Target Folder does not exist"
+    }
+
+    $source = Get-ChildItem -path $SourceFolder -recurse 
+    $target = Get-ChildItem -path $TargetFolder -recurse
+
+
+    Compare-Object -ReferenceObject $source -DifferenceObject $target
+  }
+  catch 
+  {
+    Write-Host "There was a problem comparing the folders: $_"
+  } 
+}
+
+
+Export-ModuleMember -function Compare-Folders
