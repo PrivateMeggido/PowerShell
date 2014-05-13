4ba0b0658f9e9235c22075ae380ff4b42eda7cdb
unknown <Juan_M_Medina@WN7X64-D78QFZ1.aus.amer.dell.com>
2014-05-13 14:33:58 -0500

    Added Decode function to Shell. Fumbled with gunzip function

--- Modules/Shell/Shell.psm1
+++ Modules/Shell/Shell.psm1
@@ -29,5 +29,46 @@ function Compare-Folders
   } 
 }
 
+function Decode-Base64
+{
+  param
+  (
+    [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][ValidateNotNullOrEmpty()][string] $InputText = $input
+  )
+
+  [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($InputText))
+}
+
+#function Extract-Gz
+#{
+#  param
+#  (
+#    [Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][ValidateNotNullOrEmpty()][string] $InputStream = $input
+#  )
+#
+#  Write-Host $InputStream
+#
+#  #$InputByteArray = [System.Text.Encoding].UTF8.GetBytes($InputStream)
+#
+#  #$MemoryStream = New-Object System.IO.MemoryStream($InputByteArray)
+#  $MemoryStream = New-Object System.IO.MemoryStream($InputStream)
+#
+#  $Unzipper = New-Object System.IO.Compression.GzipStream $MemoryStream, ([IO.Compression.CompressionMode]::Decompress)
+#
+#  $buffer = New-Object byte[](1024)
+#  while ($true)
+#  {
+#    $ReadBytes = $Unzipper.Read($buffer, 0, 1024)
+#
+#    if ($ReadBytes -le 0) { break }
+#
+#    Write-Output $buffer
+#  }
+#}
+
+
+
 Export-ModuleMember -function Compare-Folders
+Export-ModuleMember -function Decode-Base64
+#Export-ModuleMember -function Extract-Gz
 
